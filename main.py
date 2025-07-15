66fbc8a2431916f43d8dc15e416b093b78029cae
mattihan <mattihan@gmail.com>
2025-07-15 14:43:22 -0700

    added get_book_text function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path, encoding="utf-8") as f:
+        return f.read()
+
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+
+main()
