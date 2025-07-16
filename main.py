c3b06fac5a83e5c47be7058aa337db8200001805
mattihan <mattihan@gmail.com>
2025-07-15 17:46:39 -0700

    added print_book_report function

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_dictionary, get_character_stats
 
 
 def get_book_text(file_path):
     with open(file_path, encoding="utf-8") as f:
         return f.read()
 
+def format_counts_report(counts):
+    return '\n'.join(list(map(lambda k: f"{k["char"]}: {k["num"]}", counts)))
+
+def print_book_report(book_path):
+    book = get_book_text(book_path)
+    
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {book_path}...""")
+    print(f"""----------- Word Count ----------
+Found {get_num_words(book)} total words""")
+    character_counts = get_character_stats(book)
+    print(f"""--------- Character Count -------\n{format_counts_report(character_counts)}""")
+    print("============= END ===============")
+
 def main():
+    FRANKENSTEIN = "books/frankenstein.txt"
     try:
-        print(f"{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
-    except Exception as e:
+        print_book_report(FRANKENSTEIN)
+
+    except ValueError as e:
         print(e)
 
 
--- stats.py
+++ stats.py
@@ -2,3 +2,29 @@ def get_num_words(text):
     if type(text) != str:
         raise ValueError("the text argument must be of type str")
     return len(text.split())
+
+
+def get_character_dictionary(text):
+    if type(text) != str:
+        raise ValueError("the text argument must be of type str")
+    character_dictionary = {}
+    for character in text:
+        character = character.lower()
+        if character in character_dictionary:
+            character_dictionary[character] += 1
+        else:
+            character_dictionary[character] = 1
+
+    return character_dictionary
+
+
+def get_alphanumeric_keys(dictionary):
+    return list(filter(lambda c: c.isalpha(), sorted(dictionary.keys())))
+
+
+def get_character_stats(book):
+    dictionaries = []
+    dictionary = get_character_dictionary(book)
+    for key in get_alphanumeric_keys(dictionary):
+        dictionaries.append({"char": key, "num": dictionary[key]})
+    return dictionaries
